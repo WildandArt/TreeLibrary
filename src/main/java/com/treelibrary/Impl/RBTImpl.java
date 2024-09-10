f90fb78b124780ee2b610c4c22b5add2bf4d2021
artemy.ozerski <art.ozersky@gmail.com>
2024-09-10 05:57:37 +0300

    rbtimpl inheritance

--- src/main/java/com/treelibrary/Impl/RBTImpl.java
+++ src/main/java/com/treelibrary/Impl/RBTImpl.java
@@ -1,99 +1,10 @@
 package com.treelibrary.Impl;
-import java.util.function.Consumer;
 
-import com.treelibrary.Node;
 import com.treelibrary.RBT;
 
-public class RBTImpl<T> implements RBT<T> {
 
-    @Override
-    public Node<T> getRoot() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRoot'");
-    }
+public class RBTImpl<T extends Comparable<T>> extends BSTImpl<T> implements RBT<T> {
 
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
-    }
-
-    @Override
-    public int count() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'count'");
-    }
-
-    @Override
-    public Node<T> find(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'find'");
-    }
-
-    @Override
-    public Node<T> findMin(Node<T> root) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findMin'");
-    }
-
-    @Override
-    public Node<T> findMax(Node<T> root) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findMax'");
-    }
-
-    @Override
-    public Node<T> next(Node<T> node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'next'");
-    }
-
-    @Override
-    public Node<T> prev(Node<T> node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'prev'");
-    }
-
-    @Override
-    public void traverseInOrder(Consumer<T> action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'traverseInOrder'");
-    }
-
-    @Override
-    public void traversePreOrder(Consumer<T> action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'traversePreOrder'");
-    }
-
-    @Override
-    public void traversePostOrder(Consumer<T> action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'traversePostOrder'");
-    }
-
-    @Override
-    public int height() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'height'");
-    }
-
-    @Override
-    public T remove(Node<T> node) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
-    }
-
-    @Override
-    public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
-    }
-
-    @Override
-    public Node<T> insert(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
-    }
+    
 
 }
