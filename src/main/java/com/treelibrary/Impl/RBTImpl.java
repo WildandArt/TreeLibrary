b2fa300a707d4e7fd14213fce68ccd59785b424e
artemy.ozerski <art.ozersky@gmail.com>
2024-09-11 14:44:53 +0300

    fixImbalanceAfterInsert added

--- src/main/java/com/treelibrary/Impl/RBTImpl.java
+++ src/main/java/com/treelibrary/Impl/RBTImpl.java
@@ -69,7 +69,99 @@ public class RBTImpl<T extends Comparable<T>> extends BSTImpl<T> implements RBT<
 
     }
     private void fixImbalanceAfterInsert(RBTNode<T> node) {
+        RBTNode <T> parent = (RBTNode <T>) node.getParent();
 
+        // Case 1: Parent is null, we've reached the root, the end of the recursion
+        if (parent == null) {
+          // Uncomment the following line if you want to enforce black roots (rule 2):
+          // node.color = BLACK;
+          return;
+        }
+      
+        // Parent is black --> nothing to do
+        if (parent.isRed() == false) {
+          return;
+        }
+      
+        // From here on, parent is red
+        RBTNode <T> grandparent =  (RBTNode <T>) parent.getParent();
+      
+        // Case 2:
+        // Not having a grandparent means that parent is the root. If we enforce black roots
+        // (rule 2), grandparent will never be null, and the following if-then block can be
+        // removed.
+        if (grandparent == null) {
+          // As this method is only called on red nodes (either on newly inserted ones - or -
+          // recursively on red grandparents), all we have to do is to recolor the root black.
+          parent.setRed(false);
+          return;
+        }
+      
+        // Get the uncle (may be null/nil, in which case its color is BLACK)
+        RBTNode <T> uncle = getUncle(parent);
+      
+        // Case 3: Uncle is red -> recolor parent, grandparent and uncle
+        if (uncle != null && uncle.isRed()) {
+          parent.setRed(false);
+          grandparent.setRed(true);
+          uncle.setRed(false);
+      
+          // Call recursively for grandparent, which is now red.
+          // It might be root or have a red parent, in which case we need to fix more...
+          fixImbalanceAfterInsert(grandparent);
+        }
+      
+        // Parent is left child of grandparent
+        else if (parent == grandparent.getLeft()) {
+          // Case 4a: Uncle is black and node is left->right "inner child" of its grandparent
+          if (node == parent.getRight()) {
+            rotateLeft(parent);
+      
+            // Let "parent" point to the new root node of the rotated sub-tree.
+            // It will be recolored in the next step, which we're going to fall-through to.
+            parent = node;
+          }
+      
+          // Case 5a: Uncle is black and node is left->left "outer child" of its grandparent
+          rotateRight(grandparent);
+      
+          // Recolor original parent and grandparent
+          parent.setRed(false);
+          grandparent.setRed(true);
+        }
+      
+        // Parent is right child of grandparent
+        else {
+          // Case 4b: Uncle is black and node is right->left "inner child" of its grandparent
+          if (node == parent.getLeft()) {
+            rotateRight(parent);
+      
+            // Let "parent" point to the new root node of the rotated sub-tree.
+            // It will be recolored in the next step, which we're going to fall-through to.
+            parent = node;
+          }
+      
+          // Case 5b: Uncle is black and node is right->right "outer child" of its grandparent
+          rotateLeft(grandparent);
+      
+          // Recolor original parent and grandparent
+          parent.setRed(false);
+          grandparent.setRed(true);
+        }
+    }
+
+    private RBTNode<T> getUncle(RBTNode<T> parent) {
+
+        RBTNode<T> grandParent = (RBTNode<T>) parent.getParent();
+        if (grandParent.getLeft() == parent) {
+            return (RBTNode<T>) grandParent.getRight();
+        }
+        else if (grandParent.getRight() == parent) {
+            return (RBTNode<T>) grandParent.getLeft();
+        }
+        else {
+            throw new IllegalStateException("Parent is not a child of its grandparent");
+        }
     }
 
     public void rotateRight(RBTNode<T> node) {
